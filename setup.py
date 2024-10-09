74f009286ee7667df27a8d2b4207a8ab5517c80b
guruamit5 <guruamit5@gmail.com>
2024-10-09 23:45:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'guruamit5',
+    author_email= 'guruamit5@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
